a6cccea0fff9cd0e5543b11d7a6d28ee79ccfb80
Matias Zommer <87200156+MZommer@users.noreply.github.com>
2023-04-21 23:43:42 -0300

    Setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(name='UbiArtPY',
+      version='1',
+      description="Python Module for Handling UbiArt Framework files",
+      author='Matias Zommer',
+      url='https://github.com/MZommer/UbiArtPY',
+      packages=find_packages()
+    )
