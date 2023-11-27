13f6d92c2db651694b0c393b8f49b69f5c2068e1
Matias Zommer <87200156+MZommer@users.noreply.github.com>
2023-11-27 22:54:53 +0200

    Update __init__.py

--- UbiArtPY/__types__/__init__.py
+++ UbiArtPY/__types__/__init__.py
@@ -1,6 +1,13 @@
 from .__base__ import *
 from .Angle import Angle
-from . import JdVersion
+from .Color import Color
+from .ColorInteger import ColorInteger
+from .BasicTypeList import BasicTypeList
+from .JdVersion import JdVersion
+from .LocalisationId import LocalisationId
+from .Path import Path
+from .Platform import Platform, PlatformId, PC, X360, PS3, ORBIS, CTR, WII, EMUWII, VITA, WIIU, IPAD, DURANGO, NX, POSENET, BLAZEPOSE
+from .SafeStringID import SafeStringID
 from .String8 import String8
 from .StringID import StringID
 from .URL import URL
