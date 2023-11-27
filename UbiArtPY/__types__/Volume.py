d50f048d8edd48b4b07a4a7c79599be5425fefe6
Matias Zommer <87200156+MZommer@users.noreply.github.com>
2023-11-27 21:24:32 +0200

    Update Volume.py

--- UbiArtPY/__types__/Volume.py
+++ UbiArtPY/__types__/Volume.py
@@ -3,3 +3,13 @@ class Volume:
 
     def __init__(self, db):
         self.db = float(db)
+    
+    def __str__(self):
+        return str(self.db)
+    
+    def __eq__(self, __value) -> bool:
+        if isinstance(__value, Volume):
+            return self.db == __value.db
+        if isinstance(__value, float):
+            return self.db == __value
+        return False
