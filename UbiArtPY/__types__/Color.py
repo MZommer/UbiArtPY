e98f4e9be21b8c92cea9812fa75f91435d994b04
Matias Zommer <87200156+MZommer@users.noreply.github.com>
2023-12-03 20:07:27 +0200

    Update Color.py

--- UbiArtPY/__types__/Color.py
+++ UbiArtPY/__types__/Color.py
@@ -1,5 +1,6 @@
-from functools import singledispatchmethod
+from dataclasses import dataclass
 
+@dataclass
 class Color:
     A: int = 255
     R: int = 255
@@ -7,12 +8,19 @@ class Color:
     B: int = 255
     
     # Constructors
-    @singledispatchmethod
-    def __init__(self, value):
-        ...
-    
-    @__init__.register
-    def _(self, value: str): # Hex string
+    def __init__(self, *args):
+        print(len(args))
+        if len(args) == 1 and isinstance(args[0], str):
+            self._init_hex(args[0])
+        elif len(args) == 3:
+            self._init_rgb(*args)
+        elif len(args) == 4:
+            self._init_argb(*args)
+        else:
+            raise ValueError("Invalid color arguments")
+
+    def _init_hex(self, value):
+        # Hex string
         value = value.lstrip('#')
         length = len(value)
         if length == 3:  # RGB format
@@ -27,16 +35,14 @@ class Color:
             self.A, self.R, self.G, self.B = [int(value[i:i+2], 16) for i in range(0, 8, 2)]
         else:
             raise ValueError("Invalid hexadecimal color string")
-    
-    @__init__.register
-    def _(self, alpha: int, red: int, green: int, blue: int): # ARGB
+       
+    def _init_argb(self, alpha: int, red: int, green: int, blue: int): # ARGB
         self.A = alpha
         self.R = red
         self.G = green
         self.B = blue
-    
-    @__init__.register
-    def _(self, red: int, green: int, blue: int): # RGB
+
+    def _init_rgb(self, red: int, green: int, blue: int): # RGB
         self.A = 255
         self.R = red
         self.G = green
