db50079751e82399c11a0396a032775d30f4a505
Matias Zommer <87200156+MZommer@users.noreply.github.com>
2025-01-21 00:11:11 +0200

    Added game versions and StringID

--- UbiArtPY/__types__/Platform.py
+++ UbiArtPY/__types__/Platform.py
@@ -1,4 +1,7 @@
-from .__base__ import uint32, String8
+from .__base__ import uint32
+from .String8 import String8
+from .StringID import StringID
+from .JdVersion import *
 from enum import IntEnum
 
 class PlatformId(IntEnum):
@@ -25,31 +28,44 @@ class PlatformId(IntEnum):
 class Platform:
     Id: uint32
     Name: String8
+    AvailableVersions: tuple[JdVersion]
     
-    def __init__(self, id=None):
+    def __init__(self, id=None, available: tuple[JdVersion] = None):
         if id is None:
             self.Id = PlatformId.UNDEFINED
         elif isinstance(id, PlatformId):
             self.Id = id
+        elif isinstance(id, int):
+            try:
+                self.Id = PlatformId(id)
+            except ValueError:
+                self.Id = PlatformId.INVALID
         elif isinstance(id, str):
             self.Id = PlatformId.UNDEFINED
             for platformId in PlatformId:
                 if platformId.name.lower() == id.lower():
                     self.Id = platformId
                     break
+        if available:
+            self.AvailableVersions = available
+        else:
+            self.AvailableVersions = tuple()
     
     def is_little_endian(self):
         return self.Id in PlatformId.PC, PlatformId.X360, PlatformId.ORBIS, PlatformId.EMUWII, PlatformId.NX, PlatformId.SCARLETT, PlatformId.PROSPERO, PlatformId.BLAZEPOSE
     
     @property
     def Name(self):
-        return self.Id.name
+        return String8(self.Id.name)
 
+    def ToStringID(self):
+        return StringID(self.Name)
+    
     def __int__(self) -> uint32:
         return uint32(self.Id)
     
     def __str__(self) -> String8:
-        return String8(self.Name)
+        return self.Name
     
     def __eq__(self, __value: object) -> bool:
         if isinstance(__value, Platform):
@@ -60,20 +76,23 @@ class Platform:
             return self.Name == __value
         return False
 
-PC = Platform(PlatformId.PC)
-X360 = Platform(PlatformId.X360)
-PS3 = Platform(PlatformId.PS3)
-ORBIS = Platform(PlatformId.ORBIS)
-CTR = Platform(PlatformId.CTR)
-WII = Platform(PlatformId.WII)
-EMUWII = Platform(PlatformId.EMUWII)
-VITA = Platform(PlatformId.VITA)
-WIIU = Platform(PlatformId.WIIU)
-IPAD = Platform(PlatformId.IPAD)
-DURANGO = Platform(PlatformId.DURANGO)
-NX = Platform(PlatformId.NX)
-GGP = Platform(PlatformId.GGP)
-PROSPERO = Platform(PlatformId.PROSPERO)
-SCARLETT = Platform(PlatformId.SCARLETT)
-POSENET = Platform(PlatformId.POSENET)
-BLAZEPOSE = Platform(PlatformId.BLAZEPOSE)
+PC = Platform(PlatformId.PC, (Jd2017,))
+X360 = Platform(PlatformId.X360, (Jd2014, Jd2015, Jd2016, Jd2017, Jd2018, Jd2019))
+PS3 = Platform(PlatformId.PS3, (Jd2014, Jd2015, Jd2016, Jd2017, Jd2018))
+ORBIS = Platform(PlatformId.ORBIS, (Jd2014, Jd2015, Jd2016, Jd2017, Jd2018, Jd2019, Jd2020, Jd2021, Jd2022, JdUnlimited))
+CTR = Platform(PlatformId.CTR, (JdIgnored))
+WII = Platform(PlatformId.WII, (Jd2014, Jd2015, Jd2016, Jd2017, Jd2018, Jd2019, Jd2020))
+EMUWII = Platform(PlatformId.EMUWII, (JdIgnored))
+VITA = Platform(PlatformId.VITA, (JdIgnored))
+WIIU = Platform(PlatformId.WIIU, (Jd2014, Jd2015, Jd2016, Jd2017, Jd2018, Jd2019))
+IPAD = Platform(PlatformId.IPAD, (JdIgnored))
+DURANGO = Platform(PlatformId.DURANGO, (Jd2014, Jd2015, Jd2016, Jd2017, Jd2018, Jd2019, Jd2020, Jd2021, Jd2022, JdUnlimited))
+NX = Platform(PlatformId.NX, (Jd2017, Jd2018, Jd2019, Jd2020, Jd2021, Jd2022))
+GGP = Platform(PlatformId.GGP, (Jd2020,))
+PROSPERO = Platform(PlatformId.PROSPERO, (Jd2021, Jd2022))
+SCARLETT = Platform(PlatformId.SCARLETT, (Jd2021, Jd2022))
+POSENET = Platform(PlatformId.POSENET, (JdIgnored))
+BLAZEPOSE = Platform(PlatformId.BLAZEPOSE, (JdIgnored))
+INVALID = Platform(PlatformId.INVALID, (JdIgnored))
+
+PLATFORMS = PC, X360, PS3, ORBIS, CTR, WII, EMUWII, VITA, WIIU, IPAD, DURANGO, NX, GGP, PROSPERO, SCARLETT, POSENET, BLAZEPOSE, INVALID
