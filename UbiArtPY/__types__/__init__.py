3248bca22c94350a522baef91edd8cd2c1ec8bdb
Matias Zommer <87200156+MZommer@users.noreply.github.com>
2025-01-21 00:12:30 +0200

    Update __init__.py

--- UbiArtPY/__types__/__init__.py
+++ UbiArtPY/__types__/__init__.py
@@ -9,7 +9,7 @@ from .LocalizedPath import LocalizedPath
 from .ObjectPath import ObjectPath, Level
 from .Path import Path
 from . import Platform as Platforms
-from .Platform import Platform, PlatformId, PC
+from .Platform import Platform, PlatformId, PLATFORMS
 from .SafeStringID import SafeStringID
 from .String8 import String8
 from .StringID import StringID
