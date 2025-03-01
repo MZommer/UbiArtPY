8f5946fa813a9855d1985d31644be83490b6aa2d
Matias Zommer <87200156+MZommer@users.noreply.github.com>
2025-03-01 15:45:58 +0200

    Update FatBuilder.py

--- UbiArtPY/File/Fat/FatBuilder.py
+++ UbiArtPY/File/Fat/FatBuilder.py
@@ -1,28 +1,29 @@
-from ...__types__ import StringID, Path, String8, uint8, uint32
 from .FatConst import FILE_SIGNATURE, FILE_VERSION
+from .__types__ import FileSet, TOC8
 from ...Core import ArchiveMemory, Versioning
-from .__types__ import FileSet, TOC8, BundleSet
+from ...__types__ import StringID, Path, String8, uint8, uint32
+
 
 class FatBuilder:
-    m_files: FileSet
-    
+    files: FileSet
+
     def __init__(self) -> None:
-        self.m_files = FileSet()
-    
-    def referenceFile(self, path: Path, bundleFilename: String8) -> None:
+        self.files = {}
+
+    def reference_file(self, path: Path, bundle_filename: String8) -> None:
         path = Path(path)
-        bundleFilename = String8(bundleFilename)
-        if path not in self.m_files:
-            self.m_files[path] = BundleSet()
-        self.m_files[path].add(bundleFilename)
-    
+        bundle_filename = String8(bundle_filename)
+        if path not in self.files:
+            self.files[path] = set()
+        self.files[path].add(bundle_filename)
+
     def save(self, filename: Path) -> bool:
         retval: bool = False
         with open(filename, 'wb') as f:
-            bundleId = uint8()
-            bundleTOC = TOC8()
-            bundleIds: list[uint8] = []
-            
+            bundle_id = uint8()
+            bundle_toc: TOC8 = {}
+            bundle_ids: list[uint8] = []
+
             # FILE_SIGNATURE
             # ENGINE_SIGNATURE
             # FILE_VERSION
@@ -35,53 +36,55 @@ class FatBuilder:
             #  Count
             #  Bundle id
             #  Bundle filename
-            
+
             am = ArchiveMemory()
             signature = uint32(FILE_SIGNATURE)
             am.serialize(signature)
-            engSignature = uint32(Versioning.EngineSignature)
-            am.serialize(engSignature)
+            engine_signature = uint32(Versioning.EngineSignature)
+            am.serialize(engine_signature)
             version = uint32(FILE_VERSION)
             am.serialize(version)
 
-            size = uint32(len(self.m_files))
+            size = uint32(len(self.files))
             am.serialize(size)
-            
-            stringIDs = dict[StringID, Path]()
-            
-            for path in self.m_files.keys():
-                fileId: StringID = path.getStringID()
-                if fileId in stringIDs:
-                    raise FileExistsError(f"Duplicate StringID {fileId.GetValue()} (hash collision):\n {path}\n{stringIDs[fileId]}\nPlease rename one of these files.")
-                stringIDs[fileId] = path
-            
-            for path, bundles in self.m_files.items():
-                fileId: StringID = path.getStringID()
-                fileId.serialize(am)
-                
+
+            string_ids: dict[StringID, Path] = {}
+
+            for path in self.files.keys():
+                file_id: StringID = path.get_string_id()
+                if file_id in string_ids:
+                    raise FileExistsError(
+                        f"Duplicate StringID {file_id} (hash collision):\n {path}\n{string_ids[file_id]}\nPlease rename one of these files."
+                    )
+                string_ids[file_id] = path
+
+            for path, bundles in self.files.items():
+                file_id: StringID = path.get_string_id()
+                file_id.serialize(am)
+
                 for bundle in bundles:
-                    if bundle not in bundleTOC:
+                    if bundle not in bundle_toc:
                         # Assert to prevent integer overflow
-                        assert bundleId != 255, "Integer overflow"
-                        currentBundleId: uint8 = bundleId
-                        bundleId += 1
-                        bundleTOC[bundle] = currentBundleId
+                        assert bundle_id != 255, "Integer overflow"
+                        current_bundle_id: uint8 = bundle_id
+                        bundle_id += 1
+                        bundle_toc[bundle] = current_bundle_id
                     else:
-                        currentBundleId = bundleTOC[bundle]
-                    bundleIds.append(currentBundleId)
-                
-                size = uint32(len(bundleIds))
+                        current_bundle_id = bundle_toc[bundle]
+                    bundle_ids.append(current_bundle_id)
+
+                size = uint32(len(bundle_ids))
                 am.serialize(size)
-                for bundleId in bundleIds:
-                    am.serialize(bundleId)
-                bundleIds.clear()
-            
-            size = uint32(len(bundleTOC))
+                for bundle_id in bundle_ids:
+                    am.serialize(bundle_id)
+                bundle_ids.clear()
+
+            size = uint32(len(bundle_toc))
             am.serialize(size)
-            
-            for bundle_name, bundle_id in bundleTOC.items():
+
+            for bundle_name, bundle_id in bundle_toc.items():
                 am.serialize(bundle_id)
                 bundle_name.serialize(am)
-            
-            retval = bool(f.write(am.getData()))
+
+            retval = bool(f.write(am.get_data()))
         return retval
