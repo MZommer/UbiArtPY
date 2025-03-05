f76b04b0fd88fbc52144483be0b2766664fec448
Matias Zommer <87200156+MZommer@users.noreply.github.com>
2025-03-05 20:55:44 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@
 from setuptools import setup, find_packages
 
 setup(name='UbiArtPY',
-      version='1',
+      version='2.0.0',
       description="Python Module for Handling UbiArt Framework files",
       author='Matias Zommer',
       url='https://github.com/MZommer/UbiArtPY',
       packages=find_packages()
-    )
+      )
